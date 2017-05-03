d44aef9be9797a46d20086292753f249b054f5f1
David J Birnbaum <djbpitt@gmail.com>
2017-05-03 13:02:15 -0400

    Unwound stupid regex; using XML pull parser instead

--- collateXPrep/2017-05-03_collation.py
+++ collateXPrep/2017-05-03_collation.py
@@ -1,15 +1,15 @@
 from collatex import *
+from xml.dom import pulldom
+import string
 import re
+import json
 
-# Ignore <pb> elements
-# Ignore comments
-
-def tokenize(input):
-    # words = re.findall(r'\w+\s*|\W+', input)
-    words = re.findall(r'%|<paragraph/>|<pb.+?/>|<note>.+?</note>|<!--.+?-->|[\w</>]+\s*|[^\w%]+', input)
-    tokens = []
-    [tokens.append({"t": token}) for token in words] # create dictionaries for each token
-    return tokens
+# def tokenize(input):
+#     # words = re.findall(r'\w+\s*|\W+', input)
+#     words = re.findall(r'%|<paragraph/>|<pb.+?/>|<note>.+?</note>|<!--.+?-->|[\w</>]+\s*|[^\w% </>]+', input)
+#     tokens = []
+#     [tokens.append({"t": token}) for token in words] # create dictionaries for each token
+#    return tokens
 
 
 with open('1818_Ch1.txt', 'r') as f1818file, \
@@ -18,9 +18,4 @@ with open('1818_Ch1.txt', 'r') as f1818file, \
     f1818 = f1818file.read()
     f1823 = f1823file.read()
     f1831 = f1831file.read()
-    f1818_paragraphed = re.sub(r'\n\n',r'<paragraph/>',f1818)
-    f1818_tokens = tokenize(f1818_paragraphed)
 
-# collation = Collation()
-
-print(f1818_tokens)
