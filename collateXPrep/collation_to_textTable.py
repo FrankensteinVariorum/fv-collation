20def30b16a5aa8cbaeb20740d9c99f7f31fad16
ebeshero <ebbondar@gmail.com>
2017-05-30 16:24:21 -0400

    changed comment elements into self-closing constructions to fully mask them out of collation

--- collateXPrep/collation_to_textTable.py
+++ collateXPrep/collation_to_textTable.py
@@ -28,13 +28,14 @@ regexPageBreak = re.compile(r'<pb.+?/>')
 # Inline and block elements (with content) retained in normalization: note, hi, head, ab
 
 # GIs fall into one three classes
-# 2017-05-21 ebb: Due to trouble with pulldom parsing XML comments, I have converted these to comment elements
+# 2017-05-21 ebb: Due to trouble with pulldom parsing XML comments, I have converted these to comment elements.
+# 2017-05-30 ebb: Determined that comment elements cannot be ignored, because they have text nodes. Decided to eliminate 
 # 2017-05-22 ebb: I've set anchor elements with @xml:ids to be the indicators of collation "chunks" to process together
 ignore = ['xml', 'pb', 'comment']
 inlineEmpty = ['milestone', 'anchor', 'include']
 inlineContent = ['hi']
 blockElement = ['p', 'div', 'lg', 'l', 'head', 'note', 'ab', 'cit', 'quote', 'bibl', 'header']
-# ebb: removed 'comment', from blockElement list above, because we don't want these to be collated.
+# ebb: Tried removing 'comment', from blockElement list above, because we don't want these to be collated.
 
 def normalizeSpace(inText):
     """Replaces all whitespace spans with single space characters"""
@@ -89,10 +90,13 @@ def processWitness(inputWitness, id):
 
 for name in glob.glob('collationChunks/1818_fullFlat_*'):
     matchString = name.split("fullFlat_", 1)[1]
+    # ebb: above gets C30.xml for example
+    matchStr = matchString.split(".", 1)[0]
+    # ebb: above strips off the file extension
     with open(name, 'rb') as f1818file, \
             open('collationChunks/1823_fullFlat_' + matchString, 'rb') as f1823file, \
             open('collationChunks/1831_fullFlat_' + matchString, 'rb') as f1831file, \
-            open('teiOutput/collation_' + matchString, 'w') as outputFile:
+            open('textTableOutput/collation_' + matchStr + '.txt', 'w') as outputFile:
         f1818_tokens = regexLeadingBlankLine.sub('', regexBlankLine.sub('\n', extract(f1818file))).split('\n')
         f1823_tokens = regexLeadingBlankLine.sub('', regexBlankLine.sub('\n', extract(f1823file))).split('\n')
         f1831_tokens = regexLeadingBlankLine.sub('', regexBlankLine.sub('\n', extract(f1831file))).split('\n')
@@ -100,8 +104,10 @@ for name in glob.glob('collationChunks/1818_fullFlat_*'):
         f1823_tokenlist = processWitness(f1823_tokens, 'f1823')
         f1831_tokenlist = processWitness(f1831_tokens, 'f1831')
         collation_input = {"witnesses": [f1818_tokenlist, f1823_tokenlist, f1831_tokenlist]}
-        table = collate(collation_input, output='tei', segmentation=True)
-        # table = collate(collation_input, segmentation=True, layout='vertical')
-        print('<!-- ' + nowStr + ' -->' + table, file=outputFile)
+        # table = collate(collation_input, output='tei', segmentation=True)
+        table = collate(collation_input, segmentation=True, layout='vertical')
+        # print(nowStr + '\n' + table, file=outputFile)
+        # This yields a TypeError: "Can't convert 'AlignmentTable' object to str implicitly
+        print(table, file=outputFile)
 
 
