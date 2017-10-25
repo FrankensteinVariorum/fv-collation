31ff4f34e1ee4a487ddb7f7183dd3ab8f8cd529f
ebeshero <ebbondar@gmail.com>
2017-10-25 09:46:25 -0400

    repairing output line for sga collation to TEI

--- collateXPrep/sga_collation_to_tei.py
+++ collateXPrep/sga_collation_to_tei.py
@@ -105,7 +105,7 @@ for name in glob.glob('c56_collationChunks/1818_fullFlat_*'):
             open('c56_collationChunks/1823_fullFlat_' + matchString, 'rb') as f1823file, \
             open('c56_collationChunks/msColl_c56_' + matchString, 'rb') as fMSc56file, \
             open('c56_collationChunks/1831_fullFlat_' + matchString, 'rb') as f1831file, \
-            open('sga_teiOutput/collation_' + matchStr + '.txt', 'w') as outputFile:
+            open('sga_teiOutput/collation_' + matchStr + '.xml', 'w') as outputFile:
         fMSc56_tokens = regexLeadingBlankLine.sub('', regexBlankLine.sub('\n', extract(fMSc56file))).split('\n')
         f1818_tokens = regexLeadingBlankLine.sub('', regexBlankLine.sub('\n', extract(f1818file))).split('\n')
         f1823_tokens = regexLeadingBlankLine.sub('', regexBlankLine.sub('\n', extract(f1823file))).split('\n')
