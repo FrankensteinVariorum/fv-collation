04ff31602c94c791931bb99cfbc42a24afda1bbb
ebeshero <ebbondar@gmail.com>
2018-09-08 05:28:15 +0900

    part 2 collation of C07 subparts refined order of witnesses

--- collateXPrep/python/Part2-allWitnessIM_collation_to_xml.py
+++ collateXPrep/python/Part2-allWitnessIM_collation_to_xml.py
@@ -140,10 +140,10 @@ for name in glob.glob('../collChunks-Part2/1818_fullFlat_*'):
         matchStr = matchString.split(".", 1)[0]
         # ebb: above strips off the file extension
         with open(name, 'rb') as f1818file, \
-                open('../collChunks-Part2/1823_fullFlat_' + matchString, 'rb') as f1823file,  \
-                open('../collChunks-Part2/Thomas_fullFlat_  ' + matchString, 'rb') as  fThomasfile, \
-                open('../collChunks-Part2/msColl_ ' + matchString, 'rb') as fMSfile, \
-                open('../collChunks-Part2/1831_fullFlat_ ' + matchString, 'rb') as f1831file,  \
+                open('../collChunks-Part2/Thomas_fullFlat_' + matchString, 'rb') as fThomasfile, \
+                open('../collChunks-Part2/1823_fullFlat_' + matchString, 'rb') as f1823file, \
+                open('../collChunks-Part2/1831_fullFlat_' + matchString, 'rb') as f1831file, \
+                open('../collChunks-Part2/msColl_' + matchString, 'rb') as fMSfile, \
                 open('../Full_Part2_xmlOutput/collation_' + matchStr + '.xml', 'w') as outputFile:
                 # open('collationChunks/msColl_c56_' + matchString, 'rb') as fMSc56file, \
                 # open('collationChunks/msColl_c58_' + matchString, 'rb') as fMSc58file, \
@@ -168,7 +168,7 @@ for name in glob.glob('../collChunks-Part2/1818_fullFlat_*'):
             # fMSc57Frag_tokenlist = processWitness(fMSc57Frag_tokens, 'fMSc57Frag')
             # fMSc58Frag_tokenlist = processWitness(fMSc58Frag_tokens, 'fMSc58Frag')
 
-            collation_input = {"witnesses": [f1818_tokenlist, fThomas_tokenlist, f1823_tokenlist, f1831_tokenlist, fMS_tokenlist]}
+            collation_input = {"witnesses": [f1818_tokenlist, f1823_tokenlist, fThomas_tokenlist, fMS_tokenlist, f1831_tokenlist]}
             # table = collate(collation_input, output='tei', segmentation=True)
             # table = collate(collation_input, segmentation=True, layout='vertical')
             table = collate(collation_input, output='xml', segmentation=True)
