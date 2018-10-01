53ea63ce1d754c6646677c75cef84a2de26fc8cc
Matthew Lincoln <matthew.d.lincoln@gmail.com>
2018-10-01 16:30:28 -0400

    make this work no matter how many annotations need to be retrieved

--- hypothesis/annotation_harvesting/harvest_annotations.R
+++ hypothesis/annotation_harvesting/harvest_annotations.R
@@ -1,6 +1,8 @@
 library(httr)
 #library(rapiclient)
 library(jsonlite)
+library(glue)
+library(purrr)
 
 hyp_token <- readLines("hypothesis/annotation_harvesting/.hypothesis_token")
 
@@ -14,8 +16,19 @@ hyp_token <- readLines("hypothesis/annotation_harvesting/.hypothesis_token")
 
 fraken_group <- "GwWrAWaw"
 
-frankenstein_annotations_1 <- content(GET("https://hypothes.is/api/search?limit=200&group=GwWrAWaw", add_headers(Authorization = paste("Bearer", hyp_token))))
-frankenstein_annotations_2 <- content(GET("https://hypothes.is/api/search?limit=200&offset=200&group=GwWrAWaw", add_headers(Authorization = paste("Bearer", hyp_token))))
-frankenstein_annotations <- c(frankenstein_annotations_1[[1]], frankenstein_annotations_2[[1]])
+get_annotation_set <- function(offset = 0, limit = 200, group, token) {
+  message(glue("Fetching offset {offset}..."))
+  content(GET(glue("https://hypothes.is/api/search?offset={offset}&limit={limit}&group={fraken_group}&sort=id"), add_headers(Authorization = glue("Bearer {token}"))))
+}
+
+get_all_annotations <- function(group, token) {
+  total_annotations <- get_annotation_set(limit = 1, group = group, token = token)[[2]]
+  page_sequence <- seq(0, total_annotations, by = 200)
+  map(page_sequence, get_annotation_set, group = group, token = token) %>% 
+    map(1) %>% 
+    flatten()
+}
+
+frankenstein_annotations <- get_all_annotations(fraken_group, hyp_token)
 
 write_json(frankenstein_annotations, path = "hypothesis/annotation_harvesting/annotations.json", pretty = TRUE, auto_unbox = TRUE)
